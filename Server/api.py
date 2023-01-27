1c442a038d597a70db0f8d47b6272e486b18cee0
Oier Mentxaka <oiermentxaka@gmail.com>
2023-01-27 12:19:38 +0100

    not working login

--- Server/api.py
+++ Server/api.py
@@ -29,55 +29,63 @@ templates = Jinja2Templates(directory="templates")
 
 # HOME
 
+
 @app.get("/", response_class=HTMLResponse)
 async def uploadFile(request: Request):
-    return templates.TemplateResponse("home.html",{"request":request})
+    return templates.TemplateResponse("home.html", {"request": request})
 
-# LOGIN PAGE 
+# LOGIN PAGE
+
+
 @app.get("/login/", response_class=HTMLResponse)
-async def uploadImages(request: Request):  
-    link = "Login/login.html" 
-    return templates.TemplateResponse(link,{"request":request})
+async def uploadImages(request: Request):
+    link = "Login/login.html"
+    return templates.TemplateResponse(link, {"request": request})
 
 
-# LOGIN PAGE 
+# LOGIN PAGE
 @app.get("/confirmation/", response_class=HTMLResponse)
-async def uploadImages(request: Request):  
-    link = "Utils/confirmation.html" 
-    return templates.TemplateResponse(link,{"request":request})
+async def uploadImages(request: Request):
+    link = "Utils/confirmation.html"
+    return templates.TemplateResponse(link, {"request": request})
 
-
+# LOGIN PAGE
+@app.post("/check-password/", response_class=HTMLResponse)
+async def uploadImages(request: Request):
+    link = "Utils/confirmation.html"
+    return "correct"
 
 # UPLOADING IMAGES
 @app.get("/uploadImages/", response_class=HTMLResponse)
-async def uploadImages(request: Request):  
-    link = "Cropping/cropImages.html" 
-    return templates.TemplateResponse(link,{"request":request})
+async def uploadImages(request: Request):
+    link = "Cropping/cropImages.html"
+    return templates.TemplateResponse(link, {"request": request})
+
 
 @app.post("/cropImages/", response_class=HTMLResponse)
 async def cropImages(request: Request, files: List[UploadFile] = File(...), sessionName: str = Form(...)):
     if files:
-        session_dir = "static" + os.path.sep + "uploadedPictures" + os.path.sep + sessionName
+        session_dir = "static" + os.path.sep + \
+            "uploadedPictures" + os.path.sep + sessionName
         if not os.path.exists(session_dir):
             os.makedirs(session_dir)
         for f in files:
-            filename = "static" + os.path.sep + "uploadedPictures" + os.path.sep + sessionName + os.path.sep + f'{f.filename}'
-            with open(filename, "wb") as buffer: 
-                shutil.copyfileobj(f.file, buffer) 
-        link = "/Utils/confirmation.html" 
+            filename = "static" + os.path.sep + "uploadedPictures" + \
+                os.path.sep + sessionName + os.path.sep + f'{f.filename}'
+            with open(filename, "wb") as buffer:
+                shutil.copyfileobj(f.file, buffer)
+        link = "/Utils/confirmation.html"
     else:
         link = "Utils/rejection.html"
-    #TODO: add a THE PROCESS FOR THE TRAINING AND 
-    return None #templates.TemplateResponse(link,{"request":request})
+    # TODO: add a THE PROCESS FOR THE TRAINING AND
+    return templates.TemplateResponse(link,{"request":request})
 
 
 @app.get("/upload/", response_class=HTMLResponse)
-async def uploadImages(request: Request):  
-    link = "Cropping/uploadImages.html" 
-    return templates.TemplateResponse(link,{"request":request})
- 
+async def uploadImages(request: Request):
+    link = "Cropping/uploadImages.html"
+    return templates.TemplateResponse(link, {"request": request})
+
     #     path = "static" + os.path.sep + "ImageTransformator" + os.path.sep + "results" + os.path.sep + function + os.path.sep + file.filename.split('.')[0] + '.txt'
     #     return templates.TemplateResponse("ImageTransformator/returnText.html",{"request":request})
     # return templates.TemplateResponse("ImageTransformator/returnImage.html",{"request":request, "path": path})
-
-
