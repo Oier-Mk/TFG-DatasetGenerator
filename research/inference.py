dd2f2c45123f3842bdb7ad9f9e08f0389664e8e6
Oier Mentxaka <oiermentxaka@gmail.com>
2023-03-03 15:44:47 +0100

    delete theory

--- research/inference.py
+++ research/inference.py
@@ -12,7 +12,7 @@ def infere(model, prompt = "a car", nSteps = 20, element = "car", nImages = 1):
     _ = pipe(prompt, num_inference_steps=1)
 
     for i in range(nImages):
-        image = pipe(prompt, num_inference_steps = nSteps).images[0] #batch size num_images_per_prompt 
+        image = pipe(prompt, num_inference_steps = nSteps).images[0] #batch_size = num_images_per_prompt 
         image.save(element+" "+str(i)+".png")
 
     return True
